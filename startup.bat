d141fc4ef7ab4e4bb5b2879a8f77fb7657cf34f3
nikolai <nikolaistens@gmail.com>
2025-05-24 10:48:10 +0200

    fjerne skrift på startup

--- startup.bat
+++ startup.bat
@@ -1,5 +1,6 @@
+@echo off
+
 REM startup-skript til windows. lag en symlink i denne mappa: C:\Users\nikol\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup ved å kjøre denne kommandoen som admin i cmd: 
 REM > mklink startup.bat C:\work\dotfiles\startup.bat
 
-@echo off
 subst w: c:\work
